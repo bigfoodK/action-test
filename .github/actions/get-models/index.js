8c33fc1189f4712fe61b44fd2fcbf52979fe9ac7
bigfoodK <satcom1600@hanmail.net>
2023-03-04 17:47:20 +0000

    Test

--- .github/actions/get-models/index.js
+++ .github/actions/get-models/index.js
@@ -1,15 +1,8 @@
-const core = require("@actions/core");
-
-try {
-  const dirents = require("fs").readdirSync(".", {
-    withFileTypes: true,
-  });
-  const models = dirents
-    .filter((dirent) => dirent.isDirectory())
-    .map((directory) => directory.name)
-    .filter((directory_name) => !directory_name.startsWith("."));
-  console.log(models);
-  core.setOutput("models", models);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const dirents = require("fs").readdirSync(".", {
+  withFileTypes: true,
+});
+const models = dirents
+  .filter((dirent) => dirent.isDirectory())
+  .map((directory) => directory.name)
+  .filter((directory_name) => !directory_name.startsWith("."));
+console.log(JSON.stringify(models));
