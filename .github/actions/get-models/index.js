3ae39eb561bd4d3d6b8bba90ecb1900d80acbcd3
bigfoodK <satcom1600@hanmail.net>
2023-03-04 17:48:33 +0000

    Test

--- .github/actions/get-models/index.js
+++ .github/actions/get-models/index.js
@@ -5,4 +5,4 @@ const models = dirents
   .filter((dirent) => dirent.isDirectory())
   .map((directory) => directory.name)
   .filter((directory_name) => !directory_name.startsWith("."));
-console.log(JSON.stringify(models));
+console.log(`models=${JSON.stringify(models)}`);
