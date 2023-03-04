67b0fb49ef8ae77baca56ea7ca2f3b4021fe821a
bigfoodK <satcom1600@hanmail.net>
2023-03-04 18:00:05 +0000

    Test

--- .github/actions/get-models/index.js
+++ .github/actions/get-models/index.js
@@ -1,12 +1,8 @@
-const fs = require("fs");
-const os = require("os");
-
-const dirents = fs.readdirSync(".", {
+const dirents = require("fs").readdirSync(".", {
   withFileTypes: true,
 });
 const models = dirents
   .filter((dirent) => dirent.isDirectory())
   .map((directory) => directory.name)
   .filter((directory_name) => !directory_name.startsWith("."));
-const outputPath = process.env["GITHUB_OUTPUT"];
-fs.appendFileSync(outputPath, `models=${JSON.stringify(models)}` + os.EOL);
+console.log(`models=${JSON.stringify(models)}`);
